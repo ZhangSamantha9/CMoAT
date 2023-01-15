46d6290616715853d8edc5fedbf64fabbe0ff723
LAPTOP-SAMANTHA <zhangsamantha9@gmail.com>
2023-01-15 22:07:43 +0800

    split code structs

--- main.py
+++ main.py
@@ -12,8 +12,10 @@ proteomics = pd.DataFrame(en.get_proteomics())
 tumorProt = pd.DataFrame(en.join_metadata_to_omics(metadata_df_name="clinical", omics_df_name="proteomics",
                                       metadata_cols='Proteomics_Tumor_Normal'))
 tumor_pro_data = tumorProt[tumorProt.Proteomics_Tumor_Normal == 'Tumor']
+
 gene_cor1=tumor_pro_data.A1BG_proteomics
 gene_cor2=tumor_pro_data.EGFR_proteomics
+
 sns.set(style="darkgrid")
 plot = sns.regplot(x=gene_cor1, y=gene_cor2)
 plot.set(xlabel='A1BG', ylabel='EGFR',
