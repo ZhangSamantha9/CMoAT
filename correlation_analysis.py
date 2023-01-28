636ad47c6d50ca945746bebec48b7dff76711940
LAPTOP-SAMANTHA <zhangsamantha9@gmail.com>
2023-01-28 22:45:11 +0800

    split code structs

---
+++ correlation_analysis.py
@@ -0,0 +1,9 @@
+def log1():
+    print(1)
+
+def log2():
+    print(2)
+
+name = 'log1'
+
+locals()[name]()
--- main.py
+++ main.py
@@ -2,10 +2,20 @@ import cptac
 import cptac.utils as ut
 import pandas as pd
 import numpy as np
+from scipy import stats
 import matplotlib
 import matplotlib.pyplot as plt
 matplotlib.use('agg')
 import seaborn as sns
+import argparse
+
+cptac.list_datasets()
+
+parser = argparse.ArgumentParser(description='Calculate two gene correlation')  # 创建一个解析对象
+parser.add_argument('gene_1', type=str, help='gene_1')  # 向该对象中添加你要关注的命令行参数和选项
+parser.add_argument('gene_2', type=str, help='gene_2')
+args = parser.parse_args()
+
 
 en = cptac.Endometrial()
 proteomics = pd.DataFrame(en.get_proteomics())
@@ -15,10 +25,11 @@ tumor_pro_data = tumorProt[tumorProt.Proteomics_Tumor_Normal == 'Tumor']
 
 gene_cor1=tumor_pro_data.A1BG_proteomics
 gene_cor2=tumor_pro_data.EGFR_proteomics
+pearson_correlation_array= stats.pearsonr(gene_cor1,gene_cor2)
 
 sns.set(style="darkgrid")
 plot = sns.regplot(x=gene_cor1, y=gene_cor2)
 plot.set(xlabel='A1BG', ylabel='EGFR',
          title='gene correlation for the A1BG&EGFR gene')
 matplotlib.pyplot.savefig('gene_correlation')
-print("done")
+print(pearson_correlation_array)
