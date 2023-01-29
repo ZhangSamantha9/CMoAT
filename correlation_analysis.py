1a3094b0e01125fdb3a88347120c2f58688e23d6
126614 <zhangsamantha9@gmail.com>
2023-01-29 17:28:55 +0800

    2022.1.29

--- correlation_analysis.py
+++ correlation_analysis.py
@@ -1,9 +1,28 @@
-def log1():
-    print(1)
+import cptac
+import cptac.utils as ut
+import pandas as pd
+import numpy as np
+from scipy import stats
+import matplotlib
+import matplotlib.pyplot as plt
+matplotlib.use('agg')
+import seaborn as sns
 
-def log2():
-    print(2)
+cptac.list_datasets()
 
-name = 'log1'
+en = cptac.Endometrial()
+proteomics = pd.DataFrame(en.get_proteomics())
+tumorProt = pd.DataFrame(en.join_metadata_to_omics(metadata_df_name="clinical", omics_df_name="proteomics",
+                                      metadata_cols='Proteomics_Tumor_Normal'))
+tumor_pro_data = tumorProt[tumorProt.Proteomics_Tumor_Normal == 'Tumor']
 
-locals()[name]()
+gene_cor1=tumor_pro_data.A1BG_proteomics
+gene_cor2=tumor_pro_data.EGFR_proteomics
+pearson_correlation_array= stats.pearsonr(gene_cor1,gene_cor2)
+
+sns.set(style="darkgrid")
+plot = sns.regplot(x=gene_cor1, y=gene_cor2)
+plot.set(xlabel='A1BG', ylabel='EGFR',
+         title='gene correlation for the A1BG&EGFR gene')
+matplotlib.pyplot.savefig('gene_correlation')
+print(pearson_correlation_array)
--- main.py
+++ main.py
@@ -1,35 +1,31 @@
-import cptac
-import cptac.utils as ut
-import pandas as pd
-import numpy as np
-from scipy import stats
-import matplotlib
-import matplotlib.pyplot as plt
-matplotlib.use('agg')
-import seaborn as sns
 import argparse
 
-cptac.list_datasets()
+def list_datasets():
+    """List all available datasets."""
+
+    dataset_list_url = "https://byu.box.com/shared/static/5vwsvgu8fyzao0pb7rx8lt26huofcdax.tsv"
+
+    try:
+        dataset_list_text = _download_text(dataset_list_url)
+    except NoInternetError:
+        raise NoInternetError("Insufficient internet to download available dataset info. Check your internet connection.") from None
+
+    return pd.read_csv(io.StringIO(dataset_list_text), sep="\t", index_col=0)
+
+
+
 
 parser = argparse.ArgumentParser(description='Calculate two gene correlation')  # 创建一个解析对象
 parser.add_argument('gene_1', type=str, help='gene_1')  # 向该对象中添加你要关注的命令行参数和选项
 parser.add_argument('gene_2', type=str, help='gene_2')
 args = parser.parse_args()
 
+def log1():
+    print(1)
 
-en = cptac.Endometrial()
-proteomics = pd.DataFrame(en.get_proteomics())
-tumorProt = pd.DataFrame(en.join_metadata_to_omics(metadata_df_name="clinical", omics_df_name="proteomics",
-                                      metadata_cols='Proteomics_Tumor_Normal'))
-tumor_pro_data = tumorProt[tumorProt.Proteomics_Tumor_Normal == 'Tumor']
+def log2():
+    print(2)
 
-gene_cor1=tumor_pro_data.A1BG_proteomics
-gene_cor2=tumor_pro_data.EGFR_proteomics
-pearson_correlation_array= stats.pearsonr(gene_cor1,gene_cor2)
+name = 'log1'
 
-sns.set(style="darkgrid")
-plot = sns.regplot(x=gene_cor1, y=gene_cor2)
-plot.set(xlabel='A1BG', ylabel='EGFR',
-         title='gene correlation for the A1BG&EGFR gene')
-matplotlib.pyplot.savefig('gene_correlation')
-print(pearson_correlation_array)
+locals()[name]()
