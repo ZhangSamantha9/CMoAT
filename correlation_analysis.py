aac1450b9388c01cf2600f8df946355a82922d35
LAPTOP-SAMANTHA <zhangsamantha9@gmail.com>
2023-02-06 23:12:34 +0800

    import analysis from correlation_analysis.py

--- correlation_analysis.py
+++ correlation_analysis.py
@@ -7,22 +7,24 @@ import matplotlib
 import matplotlib.pyplot as plt
 matplotlib.use('agg')
 import seaborn as sns
+def analysis(cancername,gene1,gene2):
+    print("receive parameters:",cancername,gene1,gene2)
 
-cptac.list_datasets()
-
-en = cptac.Endometrial()
-proteomics = pd.DataFrame(en.get_proteomics())
-tumorProt = pd.DataFrame(en.join_metadata_to_omics(metadata_df_name="clinical", omics_df_name="proteomics",
-                                      metadata_cols='Proteomics_Tumor_Normal'))
-tumor_pro_data = tumorProt[tumorProt.Proteomics_Tumor_Normal == 'Tumor']
-
-gene_cor1=tumor_pro_data.A1BG_proteomics
-gene_cor2=tumor_pro_data.EGFR_proteomics
-pearson_correlation_array= stats.pearsonr(gene_cor1,gene_cor2)
-
-sns.set(style="darkgrid")
-plot = sns.regplot(x=gene_cor1, y=gene_cor2)
-plot.set(xlabel='A1BG', ylabel='EGFR',
-         title='gene correlation for the A1BG&EGFR gene')
-matplotlib.pyplot.savefig('gene_correlation')
-print(pearson_correlation_array)
+# cptac.list_datasets()
+#
+# en = cptac.Endometrial()
+# proteomics = pd.DataFrame(en.get_proteomics())
+# tumorProt = pd.DataFrame(en.join_metadata_to_omics(metadata_df_name="clinical", omics_df_name="proteomics",
+#                                       metadata_cols='Proteomics_Tumor_Normal'))
+# tumor_pro_data = tumorProt[tumorProt.Proteomics_Tumor_Normal == 'Tumor']
+#
+# gene_cor1=tumor_pro_data.A1BG_proteomics
+# gene_cor2=tumor_pro_data.EGFR_proteomics
+# pearson_correlation_array= stats.pearsonr(gene_cor1,gene_cor2)
+#
+# sns.set(style="darkgrid")
+# plot = sns.regplot(x=gene_cor1, y=gene_cor2)
+# plot.set(xlabel='A1BG', ylabel='EGFR',
+#          title='gene correlation for the A1BG&EGFR gene')
+# matplotlib.pyplot.savefig('gene_correlation')
+# print(pearson_correlation_array)
--- main.py
+++ main.py
@@ -1,24 +1,19 @@
-import argparse
-import cptac
+# import cptac
+import correlation_analysis
 
+# import argparse
+#
+#
+# parser = argparse.ArgumentParser(description='Calculate two gene correlation')
+# parser.add_argument('cancer_name', type=str, help='Choose from: Brca,Ccrcc,Colon,Endometrial,Gbm,Hnscc,Lscc,Luad,Ovarian,Pdac,UcecConf,GbmConf')
+# parser.add_argument('gene_1', type=str, help='gene 1')  # 向该对象中添加你要关注的命令行参数和选项
+# parser.add_argument('gene_2', type=str, help='gene 2')
+# args = parser.parse_args()
+#
+# cancername = args.cancer_name
+# gene1 = args.gene_1
+# gene2 = args.gene_2
 
-parser = argparse.ArgumentParser(description='Calculate two gene correlation')
-parser.add_argument('cancer_name', type=str, help='Choose from: Brca,Ccrcc,Colon,Endometrial,Gbm,Hnscc,Lscc,Luad,Ovarian,Pdac,UcecConf,GbmConf')
-parser.add_argument('gene_1', type=str, help='gene 1')  # 向该对象中添加你要关注的命令行参数和选项
-parser.add_argument('gene_2', type=str, help='gene 2')
-args = parser.parse_args()
+# correlation_analysis.analysis(cancername,gene1,gene2)
 
-def input_cancer(cancer_name):
-    cancer = cancer_name
-    return cancer
-locals()[input_cancer()]()
- #uhdihf
-def log1():
-    print(1)
-
-def log2():
-    print(2)
-
-name = 'log1'
-
-locals()[name]()
+correlation_analysis.analysis("Luad","EGFR","MET")
