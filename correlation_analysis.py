e01716a5ea90609aa10cce121ea9e9a4aa2c64f8
126614 <zhangsamantha9@gmail.com>
2023-02-13 16:51:41 +0800

    写了cancer name的函数

--- correlation_analysis.py
+++ correlation_analysis.py
@@ -7,17 +7,25 @@ import matplotlib
 import matplotlib.pyplot as plt
 matplotlib.use('agg')
 import seaborn as sns
-def analysis(cancername,gene1,gene2):
-    print("receive parameters:",cancername,gene1,gene2)
+def user_cancername(cancername):
+    print("receive parameters:",cancername)
+    if cancername == 'Endometrial' or 'endometrial':
+        en = cptac.Endometrial()
+        proteomics = pd.DataFrame(en.get_proteomics())
+        tumorProt = pd.DataFrame(en.join_metadata_to_omics(metadata_df_name="clinical", omics_df_name="proteomics",
+                                                           metadata_cols='Proteomics_Tumor_Normal'))
+        tumor_pro_data = tumorProt[tumorProt.Proteomics_Tumor_Normal == 'Tumor']
+    elif cancername == 'luad' or 'Luad':
+        Luad = cptac.Luad()
+        proteomics = pd.DataFrame(Luad.get_proteomics())
+        tumorProt = pd.DataFrame(Luad.join_metadata_to_omics(metadata_df_name="clinical", omics_df_name="proteomics",
+                                                           metadata_cols='Proteomics_Tumor_Normal'))
+        tumor_pro_data = tumorProt[tumorProt.Proteomics_Tumor_Normal == 'Tumor']
 
-# cptac.list_datasets()
-#
-# en = cptac.Endometrial()
-# proteomics = pd.DataFrame(en.get_proteomics())
-# tumorProt = pd.DataFrame(en.join_metadata_to_omics(metadata_df_name="clinical", omics_df_name="proteomics",
-#                                       metadata_cols='Proteomics_Tumor_Normal'))
-# tumor_pro_data = tumorProt[tumorProt.Proteomics_Tumor_Normal == 'Tumor']
-#
+def user_gene1(gene1):
+    print("receive parameters",gene1)
+    gene1 == globals('tumor_pro_data')['gene1']
+
 # gene_cor1=tumor_pro_data.A1BG_proteomics
 # gene_cor2=tumor_pro_data.EGFR_proteomics
 # pearson_correlation_array= stats.pearsonr(gene_cor1,gene_cor2)
