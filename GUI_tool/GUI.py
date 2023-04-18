0efef18cb4e96a9a8d3225525c3c65f0ccac4b5b
126614 <zhangsamantha9@gmail.com>
2023-04-18 16:22:21 +0800

    layout is done(don't want to improve now)

--- GUI_tool/GUI.py
+++ GUI_tool/GUI.py
@@ -2,73 +2,59 @@ import tkinter as tk
 from tkinter import ttk
 import correlation_analysis_GUI
 import os
+from PIL import ImageTk, Image
 
 
 
-class App(tk.Tk):
-    def __init__(self):
-        super().__init__()
+class protein_analysis_GUI:
+    def __init__(self,master):
+        master.title("Protein Analysis")
 
-        self.geometry("600x650")
-        self.title('Analysis')
-        self.resizable(0, 0)
-
-        # configure the grid
-        self.columnconfigure(0, weight=1)
-        self.columnconfigure(1, weight=1)
-        self.columnconfigure(2, weight=1)
+        master.geometry("650x700")
+        master.resizable(0, 0)
 
 
-        self.create_widgets()
+        text_box=ttk.Frame(master)
+        text_box.grid(sticky="W")
 
-        img = tk.PhotoImage()
-        img = img.subsample(1, 1)
-        self.image = ttk.Label(image=img)
-        self.image.image = img
-        self.image.grid(row=5)
+        correlation_label = ttk.Label(text_box, text="Gene correlation")
+        correlation_label.grid(row=0, column=0, sticky="W")
 
 
-    def create_widgets(self):
-
-        correlation_label = ttk.Label(self, text="Gene correlation")
-        correlation_label.grid(column=0, row=0, sticky=tk.W, padx=5, pady=5)
-
-        text_box=ttk.Frame(self)
-        text_box.grid()
-
-        cancer_part=ttk.Frame(text_box)
-        cancer_part.grid(row=1,columnspan=3)
-
-
-        cancername_label = ttk.Label(cancer_part, text="Cancer name:")
-        cancername_label.grid(column=0,row=1,sticky=tk.W, pady = 2)
+        cancername_label = ttk.Label(text_box, text="Cancer name:")
+        cancername_label.grid(row=1, column=0, sticky="W")
 
         self.var1 = tk.StringVar()
-        cancer_name_entry = ttk.Entry(cancer_part,textvariable=self.var1)
-        cancer_name_entry.grid(column = 2, row=1,sticky=tk.E,pady = 2)
+        cancer_name_entry = ttk.Entry(text_box,textvariable=self.var1)
+        cancer_name_entry.grid(row=1, column=1, pady=10)
 
-        gene1_part = ttk.Frame(text_box)
-        gene1_part.grid()
-
-        gene1_label = ttk.Label(gene1_part, text="Gene 1:")
-        gene1_label.grid(column=0, row=2,sticky=tk.W, pady = 2)
+        gene1_label = ttk.Label(text_box, text="Gene 1:")
+        gene1_label.grid(row=2, column=0, sticky="W")
 
         self.var2 = tk.StringVar()
-        gene1_entry = ttk.Entry(gene1_part,textvariable=self.var2)
-        gene1_entry.grid(column = 2, row =2, pady = 2)
+        gene1_entry = ttk.Entry(text_box,textvariable=self.var2)
+        gene1_entry.grid(row=2, column=1, pady=10)
 
-        gene2_part = ttk.Frame(text_box)
-        gene2_part.grid()
-        gene2_label = ttk.Label(gene2_part, text="Gene 2:")
-        gene2_label.grid(column=0, row=3,sticky=tk.W, pady = 2)
+
+        gene2_label = ttk.Label(text_box, text="Gene 2:")
+        gene2_label.grid(row=3, column=0, sticky="W")
 
         self.var3 = tk.StringVar()
-        gene2_entry = ttk.Entry(gene2_part,textvariable=self.var3)
+        gene2_entry = ttk.Entry(text_box,textvariable=self.var3)
         gene2_entry.grid(row =3, column = 1, pady = 2)
 
         # login button
         analysis_button = ttk.Button(text_box, text="Analysis", command=self.draw_correlation_curve)
-        analysis_button.grid(column=1, row=4, sticky=tk.E, padx=5, pady=5)
+        analysis_button.grid(row=4,column=0,columnspan=3,sticky="E")
+
+
+        image_box=ttk.Frame()
+        image_box.grid(sticky="W")
+        img = tk.PhotoImage()
+        img = img.subsample(1,1)
+        self.image = ttk.Label(image_box,image=img)
+        self.image.image = img
+        self.image.grid(row=5, rowspan=2, column=1, padx=20,columnspan=3)
 
     # 绘制相关性曲线
     def draw_correlation_curve(self):
@@ -80,15 +66,23 @@ class App(tk.Tk):
 
         filename= correlation_analysis_GUI.gene_correlation_curve(gene1, gene2,tumor_data_gui)
         fullpath=os.path.join(os.getcwd(),filename)+'.png'
+
+
+
         img = tk.PhotoImage(file=fullpath)
-        img = img.subsample(1, 1)
+        img = img.subsample(1,1)
         self.image.configure(image=img)
         self.image.image = img
 
 
-if __name__ == "__main__":
-    app = App()
-    app.mainloop()
+# 创建主窗口
+root = tk.Tk()
+
+# 运行GUI
+my_gui =protein_analysis_GUI(root)
+
+# 运行主循环
+root.mainloop()
 
 
 
--- GUI_tool/test.py
+++ GUI_tool/test.py
@@ -1,19 +1,56 @@
-from tkinter import *
-from tkinter import ttk
+import tkinter as tk
+from tkinter import filedialog
 
 
-root: Tk = Tk()
-root.title("Hello world")
+class MyGUI:
+    def __init__(self, master):
+        self.master = master
+        master.title("Gene Correlation Analysis")
 
-frm = ttk.Frame(root, padding=10)
-frm.grid()
-box = ttk.Frame(frm, padding=10)
-box.grid()
-ttk.Label(box, text="Hello World").grid(column=0, row=0)
-ttk.Button(box, text="Quit", command=root.destroy).grid(column=1, row=0)
+        # 创建标签和输入框
+        self.input_label = tk.Label(master, text="Select the input data file:")
+        self.input_label.grid(row=0, column=0, sticky="E")
 
-ttk.Label(frm, text="11111111111111111111111111111111111111111").grid(column=0, row=1)
+        self.input_entry = tk.Entry(master)
+        self.input_entry.grid(row=0, column=1, pady=10)
 
+        self.output_label = tk.Label(master, text="Select the output directory:")
+        self.output_label.grid(row=1, column=0, sticky="E")
+
+        self.output_entry = tk.Entry(master)
+        self.output_entry.grid(row=1, column=1, pady=10)
+
+        # 创建按钮
+        self.input_button = tk.Button(master, text="Browse", command=self.select_input_file)
+        self.input_button.grid(row=0, column=2)
+
+        self.output_button = tk.Button(master, text="Browse", command=self.select_output_directory)
+        self.output_button.grid(row=1, column=2)
+
+        self.submit_button = tk.Button(master, text="Submit", command=self.submit)
+        self.submit_button.grid(row=2, column=1, pady=20)
+
+    def select_input_file(self):
+        file_path = filedialog.askopenfilename()
+        self.input_entry.insert(0, file_path)
+
+    def select_output_directory(self):
+        directory_path = filedialog.askdirectory()
+        self.output_entry.insert(0, directory_path)
+
+    def submit(self):
+        # 在这里添加您的代码，以获取输入和输出路径并进行相应的操作
+        pass
+
+
+# 创建主窗口
+root = tk.Tk()
+
+# 运行GUI
+my_gui = MyGUI(root)
+
+# 运行主循环
 root.mainloop()
+
 
 
