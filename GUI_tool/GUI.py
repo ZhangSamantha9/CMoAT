a6cfa0c29312711f238cb879f203a91ed2df94a8
126614 <zhangsamantha9@gmail.com>
2023-04-18 14:00:01 +0800

    布局需要改善

--- GUI_tool/GUI.py
+++ GUI_tool/GUI.py
@@ -1,16 +1,8 @@
-
 import tkinter as tk
-from doctest import master
 from tkinter import ttk
 import correlation_analysis_GUI
 import os
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-from scipy import stats
-from PIL import Image, ImageTk
+
 
 
 class App(tk.Tk):
@@ -19,68 +11,65 @@ class App(tk.Tk):
 
         self.geometry("600x650")
         self.title('Analysis')
-        # self.resizable(0, 0)
+        self.resizable(0, 0)
 
         # configure the grid
         self.columnconfigure(0, weight=1)
-        self.columnconfigure(1, weight=3)
-        # self.root = root
+        self.columnconfigure(1, weight=1)
+        self.columnconfigure(2, weight=1)
+
 
         self.create_widgets()
 
         img = tk.PhotoImage()
-        # img = tk.PhotoImage(file="C:/Users/SUISUISHOU/Documents/Python/gene_correlation/GUI_tool/EGFR and MET correlation curve.png")
         img = img.subsample(1, 1)
         self.image = ttk.Label(image=img)
         self.image.image = img
         self.image.grid(row=5)
-        # img1 = tk.PhotoImage(file="C:/Users/SUISUISHOU/Documents/Python/gene_correlation/GUI_tool/EGFR and MET correlation curve.png")
 
 
     def create_widgets(self):
-        # username
+
         correlation_label = ttk.Label(self, text="Gene correlation")
         correlation_label.grid(column=0, row=0, sticky=tk.W, padx=5, pady=5)
 
-        cancername_label = ttk.Label(self, text="Cancer name:")
-        cancername_label.grid(column=0, row=1, sticky=tk.W, padx=5, pady=5)
+        text_box=ttk.Frame(self)
+        text_box.grid()
+
+        cancer_part=ttk.Frame(text_box)
+        cancer_part.grid(row=1,columnspan=3)
+
+
+        cancername_label = ttk.Label(cancer_part, text="Cancer name:")
+        cancername_label.grid(column=0,row=1,sticky=tk.W, pady = 2)
 
         self.var1 = tk.StringVar()
-        cancer_name_entry = ttk.Entry(self,textvariable=self.var1)
-        cancer_name_entry.grid(column=1, row=1, sticky=tk.E, padx=5, pady=5)
+        cancer_name_entry = ttk.Entry(cancer_part,textvariable=self.var1)
+        cancer_name_entry.grid(column = 2, row=1,sticky=tk.E,pady = 2)
 
-        # password
-        gene1_label = ttk.Label(self, text="Gene 1:")
-        gene1_label.grid(column=0, row=2, sticky=tk.W, padx=5, pady=5)
+        gene1_part = ttk.Frame(text_box)
+        gene1_part.grid()
+
+        gene1_label = ttk.Label(gene1_part, text="Gene 1:")
+        gene1_label.grid(column=0, row=2,sticky=tk.W, pady = 2)
 
         self.var2 = tk.StringVar()
-        gene1_entry = ttk.Entry(self,textvariable=self.var2)
-        gene1_entry.grid(column=1, row=2, sticky=tk.E, padx=5, pady=5)
+        gene1_entry = ttk.Entry(gene1_part,textvariable=self.var2)
+        gene1_entry.grid(column = 2, row =2, pady = 2)
 
-        gene2_label = ttk.Label(self, text="Gene 2:")
-        gene2_label.grid(column=0, row=3, sticky=tk.W, padx=5, pady=5)
+        gene2_part = ttk.Frame(text_box)
+        gene2_part.grid()
+        gene2_label = ttk.Label(gene2_part, text="Gene 2:")
+        gene2_label.grid(column=0, row=3,sticky=tk.W, pady = 2)
 
         self.var3 = tk.StringVar()
-        gene2_entry = ttk.Entry(self,textvariable=self.var3)
-        gene2_entry.grid(column=1, row=3, sticky=tk.E, padx=5, pady=5)
-
+        gene2_entry = ttk.Entry(gene2_part,textvariable=self.var3)
+        gene2_entry.grid(row =3, column = 1, pady = 2)
 
         # login button
-        analysis_button = ttk.Button(self, text="Analysis", command=self.draw_correlation_curve)
+        analysis_button = ttk.Button(text_box, text="Analysis", command=self.draw_correlation_curve)
         analysis_button.grid(column=1, row=4, sticky=tk.E, padx=5, pady=5)
 
-
-
-        # self.figure = self.draw_correlation_curve()
-        #
-        # self.canvas = FigureCanvasTkAgg(self.figure, self.root)
-        # self.canvas.get_tk_widget().pack()
-
-
-        # R_label = ttk.Label(self, text='r')
-        # R_label.grid(column=0, row=6, sticky=tk.W, padx=5, pady=5)
-        # pvalue_label = ttk.Label(self, text='p')
-        # pvalue_label.grid(column=0, row=7, sticky=tk.W, padx=5, pady=5)
     # 绘制相关性曲线
     def draw_correlation_curve(self):
         cancer_name=self.var1.get()
@@ -95,16 +84,8 @@ class App(tk.Tk):
         img = img.subsample(1, 1)
         self.image.configure(image=img)
         self.image.image = img
-        # sns.set(style="darkgrid")
-        # gene_corrlation_plot = sns.regplot(x=gene1_data, y=gene2_data)
-        # gene_corrlation_plot.set(xlabel=str(self.var2), ylabel=str(self.var3))
-        # return  gene_corrlation_plot
 
 
-# if __name__ == '__main__':
-#     root = tk.Tk()
-#     # app=App()
-#     root.mainloop()
 if __name__ == "__main__":
     app = App()
     app.mainloop()
--- GUI_tool/test.py
+++ GUI_tool/test.py
@@ -1,42 +1,19 @@
-import tkinter as tk
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from tkinter import *
+from tkinter import ttk
 
 
-class CorrelationCurveApp:
+root: Tk = Tk()
+root.title("Hello world")
 
-    def __init__(self, root):
-        self.root = root
-        self.root.title("Correlation Curve")
+frm = ttk.Frame(root, padding=10)
+frm.grid()
+box = ttk.Frame(frm, padding=10)
+box.grid()
+ttk.Label(box, text="Hello World").grid(column=0, row=0)
+ttk.Button(box, text="Quit", command=root.destroy).grid(column=1, row=0)
 
-        # Define X and Y data
-        self.x = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9])
-        self.y = np.array([2, 4, 5, 7, 9, 10, 12, 14, 15])
+ttk.Label(frm, text="11111111111111111111111111111111111111111").grid(column=0, row=1)
 
-        self.button = tk.Button(self.root, text="Draw", command=self.draw_correlation_curve)
-        self.button.pack()
-
-        self.correlation_coefficient_label = tk.Label(self.root, text="")
-        self.correlation_coefficient_label.pack()
-
-        self.figure = plt.Figure(figsize=(5, 4), dpi=100)
-        self.plot = self.figure.add_subplot(111)
-
-        self.canvas = FigureCanvasTkAgg(self.figure, self.root)
-        self.canvas.get_tk_widget().pack()
-
-    def draw_correlation_curve(self):
-        self.plot.clear()
-        corr_coef = np.corrcoef(self.x, self.y)[0][1]
-        self.correlation_coefficient_label.config(text="Correlation Coefficient: {:.2f}".format(corr_coef))
-        self.plot.scatter(self.x, self.y)
-        self.canvas.draw()
-
-
-if __name__ == '__main__':
-    root = tk.Tk()
-    app = CorrelationCurveApp(root)
-    root.mainloop()
+root.mainloop()
 
 
