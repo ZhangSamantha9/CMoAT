c735c502966144973f3d45d768fea6f78e130311
126614 <zhangsamantha9@gmail.com>
2023-04-19 10:36:10 +0800

    learn about ttk.notebook

--- GUI_tool/test.py
+++ GUI_tool/test.py
@@ -1,53 +1,31 @@
 import tkinter as tk
-from tkinter import filedialog
-
-
-class MyGUI:
-    def __init__(self, master):
-        self.master = master
-        master.title("Gene Correlation Analysis")
-
-        # 创建标签和输入框
-        self.input_label = tk.Label(master, text="Select the input data file:")
-        self.input_label.grid(row=0, column=0, sticky="E")
-
-        self.input_entry = tk.Entry(master)
-        self.input_entry.grid(row=0, column=1, pady=10)
-
-        self.output_label = tk.Label(master, text="Select the output directory:")
-        self.output_label.grid(row=1, column=0, sticky="E")
-
-        self.output_entry = tk.Entry(master)
-        self.output_entry.grid(row=1, column=1, pady=10)
-
-        # 创建按钮
-        self.input_button = tk.Button(master, text="Browse", command=self.select_input_file)
-        self.input_button.grid(row=0, column=2)
-
-        self.output_button = tk.Button(master, text="Browse", command=self.select_output_directory)
-        self.output_button.grid(row=1, column=2)
-
-        self.submit_button = tk.Button(master, text="Submit", command=self.submit)
-        self.submit_button.grid(row=2, column=1, pady=20)
-
-    def select_input_file(self):
-        file_path = filedialog.askopenfilename()
-        self.input_entry.insert(0, file_path)
-
-    def select_output_directory(self):
-        directory_path = filedialog.askdirectory()
-        self.output_entry.insert(0, directory_path)
-
-    def submit(self):
-        # 在这里添加您的代码，以获取输入和输出路径并进行相应的操作
-        pass
-
+from tkinter import ttk
 
 # 创建主窗口
 root = tk.Tk()
+root.title("My Notebook")
 
-# 运行GUI
-my_gui = MyGUI(root)
+# 创建ttk.Notebook小部件
+notebook = ttk.Notebook(root)
+
+# 创建第一个选项卡
+tab1 = ttk.Frame(notebook)
+notebook.add(tab1, text="Tab 1")
+
+# 在第一个选项卡中添加一个标签
+label1 = ttk.Label(tab1, text="This is tab 1!")
+label1.pack(padx=10, pady=10)
+
+# 创建第二个选项卡
+tab2 = ttk.Frame(notebook)
+notebook.add(tab2, text="Tab 2")
+
+# 在第二个选项卡中添加一个按钮
+button1 = ttk.Button(tab2, text="Click me!")
+button1.pack(padx=10, pady=10)
+
+# 将ttk.Notebook小部件添加到主窗口中
+notebook.pack(expand=True, fill="both")
 
 # 运行主循环
 root.mainloop()
