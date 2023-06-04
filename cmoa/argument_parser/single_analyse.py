3294fac95a44762ccc8229d61eb8f902aa32dd00
suisuishou <blightue@gmail.com>
2023-06-04 18:39:17 +0800

    Exception catch

--- cmoa/argument_parser/single_analyse.py
+++ cmoa/argument_parser/single_analyse.py
@@ -5,11 +5,8 @@ from typing import Optional
 from cmoa.libs.analysis_tasks.correlation_analysis_task import CorrelationAnalysisTask
 
 def correlation_analysis(cancer_name: str, gene1: str, gene2: str) -> Optional[str]:
-    cancer_data = cd.load_dataset(cancer_name)
-    if cancer_data:
-        # tumor_data = ca.dataset_preprocess(cancer_data)
-        # return ca.draw_correlation_curve(gene1, gene2, tumor_data)
+    try:
         task = CorrelationAnalysisTask(cancer_name, gene1, gene2)
         return task.run_task()
-    else:
-        print(f'Load dataset {cancer_name} failed.')
+    except Exception:
+        return None
