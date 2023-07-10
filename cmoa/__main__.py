6dac1d241aae43887f5d6f902975842660476429
suisuishou <blightue@gmail.com>
2023-07-10 22:18:35 +0800

    Add cli sample, Add ca fully argument

--- cmoa/__main__.py
+++ cmoa/__main__.py
@@ -4,11 +4,13 @@ import cmoa.argument_parser.GUI as GUI
 
 # Single example: --ca Luad EGFR MET
 
+# CLI example: python -m cmoa -ca Luad EGFR MET 
+
 parser = argparse.ArgumentParser(description='CMOA Cli')
 parser.add_argument('-m', '--mode', type=str, default='single', choices=['single', 'batch', 'GUI'], help='single_cli, batch_cli or gui')
 exclusive_group = parser.add_mutually_exclusive_group()
 
-exclusive_group.add_argument('--ca', nargs=3, metavar=('cancer_name', 'gene1', 'gene2'), help='Gene corralation analysis, args: [cancer_name] [gene1] [gene2] e.g. --ca Luad EGFR MET')
+exclusive_group.add_argument('--cor_analy' '-ca', nargs=3, metavar=('cancer_name', 'gene1', 'gene2'), help='Gene corralation analysis, args: [cancer_name] [gene1] [gene2] e.g. --ca Luad EGFR MET')
 
 args = parser.parse_args()
 
