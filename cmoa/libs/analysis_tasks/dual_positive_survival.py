5e15d9f1868e96b8a58f2250df09e283370d13b7
126614 <zhangsamantha9@gmail.com>
2023-07-19 16:56:24 +0800

    调试，发现很多问题 1.数据库格式不一致（蛋白名字） 2.有些数据库没有follow——up数据 3.数据缺失

--- cmoa/libs/analysis_tasks/dual_positive_survival.py
+++ cmoa/libs/analysis_tasks/dual_positive_survival.py
@@ -24,7 +24,9 @@ class DualSurvivalAnalysisTask(AnalysisTaskBase):
       # if not self.cancer_dataset :
       #      raise PreprocessError(f'cancer dataset [{self.cancer_name}] load failure. ')
       cancer_data=self.cancer_dataset
-      # cancer_data.to_excel('cancer_dataset.xlsx')
+      #cancer_data.to_excel('cancer_dataset.xlsx')
+
+
       print('cancer data')
 
 
@@ -34,9 +36,13 @@ class DualSurvivalAnalysisTask(AnalysisTaskBase):
 
         clinical = self.cancer_dataset.get_clinical()
         cols = list(clinical.columns)
-        omics_gene1 = self.gene1_name
-        omics_gene2 = self.gene2_name
+        tail = '_proteomics'
+        omics_gene1 = self.gene1_name+tail
+        omics_gene2 = self.gene2_name+tail
+
+        print('00')
         follow_up = self.cancer_dataset.get_followup()
+        print('000')
         clinical.to_excel('clinical.xlsx')
         follow_up.to_excel('follow_up.xlsx')
         print('1')
@@ -47,6 +53,8 @@ class DualSurvivalAnalysisTask(AnalysisTaskBase):
             omics_genes=omics_gene1,
             quiet=True
         )
+        print('0')
+        cancer_raw_data1 = cancer_raw_data1.iloc[:, :17]
         cancer_raw_data2 = self.cancer_dataset.join_metadata_to_omics(
             metadata_df_name="clinical",
             omics_df_name="proteomics",
@@ -54,19 +62,31 @@ class DualSurvivalAnalysisTask(AnalysisTaskBase):
             omics_genes=omics_gene2,
             quiet=True
         )
+        cancer_raw_data2 = cancer_raw_data2.iloc[:, :17]
+        if omics_gene1 not in cancer_raw_data1.column:
+               raise ProcessError(f'Gene [{omics_gene1}] not in dataframe')
+        if omics_gene2 not in cancer_raw_data2.column:
+             raise ProcessError(f'Gene [{omics_gene2}] not in dataframe')
+
         follow_up = follow_up.rename({'PPID': 'Patient_ID'}, axis='columns')
         cancer_raw_data1.to_excel('cancer_raw_data1.xlsx')
+        print('3')
 
         reduced_cancer_data1 = pd.DataFrame(
             ut.reduce_multiindex(
                 df=cancer_raw_data1,
                 levels_to_drop="Database_ID"
             ))
+        # reduced_cancer_data1 = pd.DataFrame(cancer_raw_data1)
+
         reduced_cancer_data2 = pd.DataFrame(
             ut.reduce_multiindex(
                 df=cancer_raw_data2,
                 levels_to_drop="Database_ID"
             ))
+        # reduced_cancer_data2 = pd.DataFrame(cancer_raw_data2)
+        print('4')
+
         reduced_cancer_data2.to_excel('reduced_cancer_data2.xlsx')
 
 
@@ -76,6 +96,7 @@ class DualSurvivalAnalysisTask(AnalysisTaskBase):
         clin_prot_follow2.to_excel('clin_prot_follow2.xlsx')
         clin_prot_follow= pd.merge(clin_prot_follow1,clin_prot_follow2)
         clin_prot_follow.to_excel('clin_prot_follow.xlsx')
+        print('5')
         columns_to_focus_on = ['Vital Status',
                                     'Path Diag to Last Contact(Day)',
                                     'Path Diag to Death(days)']
@@ -85,19 +106,19 @@ class DualSurvivalAnalysisTask(AnalysisTaskBase):
 
         tail= '_proteomics'
         gene1_proteomics_name = self.gene1_name+tail
-        gene2_proteomics_name = self.gene2_name + tail
+        gene2_proteomics_name = self.gene2_name+tail
 
 
-        # if gene1_proteomics_name not in clin_prot_follow.column:
-        #     raise ProcessError(f'Gene [{gene1_proteomics_name}] not in dataframe')
-        # if gene2_proteomics_name not in clin_prot_follow.column:
-        #     raise ProcessError(f'Gene [{gene1_proteomics_name}] not in dataframe')
+
 
         columns_to_focus_on.append(gene1_proteomics_name)
+        print('5.1')
         columns_to_focus_on.append(gene2_proteomics_name)
+        print('5.2')
         focus_group = clin_prot_follow[columns_to_focus_on].copy()
         # focus_group2 = clin_prot_follow2[columns_to_focus_on].copy()
         focus_group = focus_group.copy()
+        print('6')
         # focus_group2 = focus_group2.copy()
 
         focus_group['Vital Status'] = focus_group['Vital Status'].replace('Living', False)
@@ -111,6 +132,7 @@ class DualSurvivalAnalysisTask(AnalysisTaskBase):
 
 
         focus_group = focus_group.assign(Days_Until_Last_Contact_Or_Death=focus_group[cols].sum(1)).drop(cols, 1)
+        # focus_group = focus_group.iloc[:, 1:4]
         focus_group.to_excel('focus_group.xlsx')
         print('2')
         lower_25_filter = (focus_group[gene1_proteomics_name] <= focus_group[gene1_proteomics_name].quantile(.25))\
@@ -145,11 +167,11 @@ class DualSurvivalAnalysisTask(AnalysisTaskBase):
         groups = df_clean[gene1_proteomics_name]
         ix = (groups == 'Lower_25%')
         kmf.fit(T[~ix], E[~ix], label='Upper_75%')
-        plot = kmf.plot_survival_function(loc=slice(0., 1500.))
+        plot = kmf.plot_survival_function(loc=slice(0., 1100.))
 
         kmf.fit(T[ix], E[ix], label='Lower_25%')
-        plot = kmf.plot_survival_function(ax=plot, loc=slice(0., 1500.))
-        figPath = os.path.join(os.getcwd(), f'[{gene1_proteomics_name}] and [{gene2_proteomics_name}] survival.png')
+        plot = kmf.plot_survival_function(ax=plot, loc=slice(0., 1100.))
+        figPath = os.path.join(os.getcwd(), f'[{gene1_proteomics_name}] and [{gene2_proteomics_name}] survival of [{self.cancer_name}].png')
         plot.get_figure().savefig(figPath)
 
         if (os.path.exists(figPath)):
