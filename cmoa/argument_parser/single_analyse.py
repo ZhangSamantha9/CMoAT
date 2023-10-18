7deac67f7845a38638fabbb448df56b184c263af
suisuishou <blightue@gmail.com>
2023-10-18 14:32:32 +0800

    Fixed not raise process exception issue

--- cmoa/argument_parser/single_analyse.py
+++ cmoa/argument_parser/single_analyse.py
@@ -8,38 +8,27 @@ from cmoa.libs.analysis_tasks.survival_plot import SurvivalAnalysisTask
 from cmoa.libs.analysis_tasks.dual_positive_survival import DualSurvivalAnalysisTask
 from cmoa.libs.analysis_tasks.normal_tissue_task import NormalTissueTask
 
+
 def correlation_analysis(cancer_name: str, gene1: str, gene2: str) -> Optional[str]:
-    try:
-        task = CorrelationAnalysisTask(cancer_name, gene1, gene2)
-        return task.run_task()
-    except Exception:
-        return None
+    task = CorrelationAnalysisTask(cancer_name, gene1, gene2)
+    return task.run_task()
+
 
 def expression_boxplot_analysis(cancer_name: str, gene: str) -> Optional[str]:
-    try:
-        task = ExpressionBoxplotTask(cancer_name, gene)
-        return task.run_task()
-    except Exception:
-        return None
-    
+    task = ExpressionBoxplotTask(cancer_name, gene)
+    return task.run_task()
+
+
 def normal_tissue_analysis(geneIds: list[str]) -> Optional[str]:
-    try:
-        task = NormalTissueTask(geneIds)
-        return task.run_task()
-    except Exception:
-        return None
+    task = NormalTissueTask(geneIds)
+    return task.run_task()
+
 
 def survival_analysis(cancer_name: str, gene: str) -> Optional[str]:
-    try:
-        task = SurvivalAnalysisTask(cancer_name, gene)
-        return task.run_task()
-    except Exception:
-        return None
+    task = SurvivalAnalysisTask(cancer_name, gene)
+    return task.run_task()
 
-def dual_survival_analysis(cancer_name: str, gene1: str,gene2:str) -> Optional[str]:
-    try:
-        task = DualSurvivalAnalysisTask(cancer_name, gene1,gene2)
-        return task.run_task()
-    except Exception:
-        return None
 
+def dual_survival_analysis(cancer_name: str, gene1: str, gene2: str) -> Optional[str]:
+    task = DualSurvivalAnalysisTask(cancer_name, gene1, gene2)
+    return task.run_task()
