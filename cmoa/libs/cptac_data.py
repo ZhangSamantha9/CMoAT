f1153097a9651ce5d7cec33b69a3d698056ab6d7
suisuishou <blightue@gmail.com>
2023-10-24 11:00:19 +0800

    update cptac 1.5.5 data load api

--- cmoa/libs/cptac_data.py
+++ cmoa/libs/cptac_data.py
@@ -1,29 +1,44 @@
 import cptac
 from typing import Optional
 
-from cptac import dataset
+from cptac.cancers.cancer import Cancer
 
 
 CANCER_DATASET_DIC = {
     'Luad': cptac.Luad,
-    'Endometrial': cptac.Endometrial,
+    # 'Endometrial': cptac.Endometrial,
     'Brca': cptac.Brca,
     'Ccrcc': cptac.Ccrcc,
-    'Colon': cptac.Colon,
+    'Coad': cptac.Coad,
     'Gbm': cptac.Gbm,
     'Hnscc': cptac.Hnscc,
     'Lscc': cptac.Lscc,
-    'Ovarian': cptac.Ovarian,
+    'Ov': cptac.Ov,
     'Pdac': cptac.Pdac,
-    'UcecConf': cptac.UcecConf,
-    'GbmConf': cptac.GbmConf
+    'Ucec': cptac.Ucec,
+    # 'GbmConf': cptac.GbmConf
 }
 
-def load_dataset(cancer_name) -> Optional[dataset.Dataset]:
+# {
+#     # 'brca': 'Breast invasive carcinoma',
+#     # 'ccrcc': 'Clear cell renal cell carcinoma',
+#     # 'coad': 'Colon adenocarcinoma',
+#     # 'gbm': 'Glioblastoma multiforme',
+#     # 'hnscc': 'Head and Neck squamous cell carcinoma',
+#     # 'lscc': 'Lung squamous cell carcinoma',
+#     # 'luad': 'Lung adenocarcinoma',
+#     # 'ov': 'Ovarian serous cystadenocarcinoma',
+#     'pda': 'Pancreatic ductal adenocarcinoma',
+#     # 'pdac': 'Pancreatic ductal adenocarcinoma',
+#     # 'ucec': 'Uterine Corpus Endometrial Carcinoma'
+# }
+
+
+def load_dataset(cancer_name) -> Optional[Cancer]:
     print(f'loading {cancer_name} data')
 
     if CANCER_DATASET_DIC.__contains__(cancer_name):
-        cptac.download(dataset=cancer_name, version='latest')
+        # cptac.download(dataset=cancer_name, version='latest')
         return CANCER_DATASET_DIC[cancer_name]()
     else:
         return None
